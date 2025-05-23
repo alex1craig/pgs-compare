e71dd66df5b198af1d98625ff4b556a79d71962b
alexcraig043 <alexcraig043@gmail.com>
2025-05-22 20:50:20 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pgs_compare",
-    version="0.2.0",
+    version="0.2.1",
     author="Alexander Craig",
     description="Compare PGS scores across ancestry groups",
     long_description=long_description,
